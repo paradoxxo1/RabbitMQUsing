0ce67238f3cb78024653b573b7252b22e39349aa
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2024-06-26 19:54:35 +0300

    PublishSubscribe
    PublishSubscribe

--- WorkQueues/WorkQueues.NewTask/Program.cs
+++ WorkQueues/WorkQueues.NewTask/Program.cs
@@ -18,20 +18,24 @@ internal class Program
         using var channel = connection.CreateModel();
 
         channel.QueueDeclare(
-            queue: "hello",
-            durable: false,
+            queue: "task_queue",
+            durable: true,
             exclusive: false,
             autoDelete: false,
             arguments: null
            );
 
+
         string message = GetMessage(args);
         var body = Encoding.UTF8.GetBytes(message);
 
+        var properties = channel.CreateBasicProperties();
+        properties.Persistent = true;
+
         channel.BasicPublish(
             exchange: string.Empty,
-            routingKey: "hello",
-            basicProperties: null,
+            routingKey: "task_queue",
+            basicProperties: properties,
             body: body);
 
         Console.WriteLine($" [*] Send {message}");
--- WorkQueues/WorkQueues.Worker/Program.cs
+++ WorkQueues/WorkQueues.Worker/Program.cs
@@ -13,11 +13,13 @@ internal class Program
         var channel = connection.CreateModel();
 
         channel.QueueDeclare(
-            queue: "hello",
-            durable: false,
+            queue: "task_queue",
+            durable: true,
             exclusive: false,
             autoDelete: false,
             arguments: null);
+
+        channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
         Console.WriteLine(" [*] Waiting for messages.");
 
@@ -32,9 +34,11 @@ internal class Program
             Thread.Sleep(dots * 1000);
 
             Console.WriteLine("[x] Done");
+
+            channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
         };
 
-        channel.BasicConsume(queue: "hello", autoAck: false, consumer: consumer);
+        channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
 
         Console.WriteLine(" Press [enter] to exit");
 
