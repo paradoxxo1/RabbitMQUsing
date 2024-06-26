9f3f1897ba7c0c2451969f2b6a9fde920109ad58
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2024-06-26 19:21:24 +0300

    Work Queues
    Work Queues

---
+++ HelloWorld/HelloWorld.Send/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace HelloWorld.Send;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(" Press [enter] to send message");
+        Console.ReadLine();
+
+        var factory = new ConnectionFactory();
+        factory.HostName = "localhost";
+        factory.Port = 5672;
+        factory.UserName = "guest";
+        factory.Password = "guest";
+
+        //factory.Uri = new Uri("amqps://qguyrlsf:F-lLRC0MUmn6vTkSC28B8HmH7hrRt2Oq@rat.rmq2.cloudamqp.com/qguyrlsf");
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(
+            queue: "hello",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+           );
+
+        string message = "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(
+            exchange: string.Empty,
+            routingKey: "hello",
+            basicProperties: null,
+            body: body);
+
+        Console.WriteLine($" [*] Send {message}");
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
+
+
---
+++ WorkQueues/WorkQueues.NewTask/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace WorkQueues.NewTask;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.HostName = "localhost";
+        factory.Port = 5672;
+        factory.UserName = "guest";
+        factory.Password = "guest";
+
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(
+            queue: "hello",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+           );
+
+        string message = GetMessage(args);
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(
+            exchange: string.Empty,
+            routingKey: "hello",
+            basicProperties: null,
+            body: body);
+
+        Console.WriteLine($" [*] Send {message}");
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+
+    static string GetMessage(string[] args)
+    {
+        return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+    }
+}
